8588144e3a028599b5a4b9f9e350a1c99f118fe5
Patrik Johnson <patrik@zebros.fi>
2016-08-06 15:59:48 +0300

    Add POC initial version of autoOpen for '(' character

--- misbehave.js
+++ misbehave.js
@@ -4,6 +4,23 @@ import Combokeys from 'combokeys'
 const log = (e, combo) => { console.log(combo) }
 const block = (e, combo) => { console.log(combo); return false; }
 
+
+const insert = (str, index, s) => { return str.slice(0, index) + s + str.slice(index) }
+const withSelection = (fn) => () => {
+  let sel = window.getSelection()
+  let range = sel.getRangeAt(0)
+  return fn(sel, range, range.startOffset, range.endOffset)
+}
+const setSelection = (elem, start, end) => {
+  let range = document.createRange()
+  range.setStart(elem, start)
+  range.setEnd(elem, end)
+
+  let selection = window.getSelection()
+  selection.removeAllRanges()
+  selection.addRange(range)
+}
+
 let code = {
 
   oninit : ({ state }) => {
@@ -19,11 +36,15 @@ let code = {
     state.keys.bind('backspace', log)
     state.keys.bind('del', log)
     state.keys.bind('enter', log)
-    state.keys.bind('(', () => {
-      state.content(state.content() + '()')
+    state.keys.bind('(', withSelection((select, range, startOffset, endOffset) => {
+      let update = insert(state.content(), startOffset, '(')
+      update = insert(update, endOffset + 1 , ')')
+      state.content(update)
       dom.textContent = state.content()
+
+      setSelection(dom.firstChild, startOffset + 1, endOffset + 1)
       return false
-    })
+    }))
     state.keys.bind('[', log)
     state.keys.bind('{', log)
     state.keys.bind("'", log)
