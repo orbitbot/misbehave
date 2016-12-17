09c1b341056531033bad2bbe098f3d3f5f78b735
Patrik Johnson <patrik@zebros.fi>
2016-12-18 00:11:28 +0200

    Update selection utils to version that handles textnodes instead of plain text content

--- utils/selection.js
+++ utils/selection.js
@@ -1,48 +1,92 @@
 "use strict"
 
-import { onNewLine } from './utils'
+export const getSections = (elem, callback) => {
+  var sel, range, tempRange, prefix = '', selected = '', suffix = ''
 
-export let getLinePosition = (node) => {
-  let text = ''
-  let sibling = node.previousSibling
-  while (sibling) {
-    text += sibling.textContent
-    sibling = sibling.previousSibling
-  }
-  return text.split(onNewLine).length - 1
-}
-
-export let withSelection = (fn) => () => {
-  let sel = window.getSelection()
-  return fn(sel, sel.getRangeAt(0), getLinePosition(sel.anchorNode), sel.anchorOffset, getLinePosition(sel.focusNode), sel.focusOffset)
-}
-
-export let withStartEnd = (fn) => {
-  return withSelection((selection, range, anchorLine, anchorOffset, focusLine, focusOffset) => {
-    // console.log(`start l,r ${ anchorLine + ' ' + anchorOffset } end l,r ${ focusLine + ' ' + focusOffset }`)
-    // calls fn with (selection, range, startLine, startOffset, endLine, endOffset)
-    if (anchorLine == focusLine) {
-      return fn(selection, range, anchorLine, Math.min(anchorOffset, focusOffset), anchorLine, Math.max(anchorOffset, focusOffset))
-    } else if (anchorLine < focusLine) {
-      return fn(selection, range, anchorLine, anchorOffset, focusLine, focusOffset)
+  if (typeof window.getSelection !== 'undefined') {
+    sel = window.getSelection()
+    selected = sel.toString()
+    if (sel.rangeCount) {
+      range = sel.getRangeAt(0)
     } else {
-      return fn(selection, range, focusLine, focusOffset, anchorLine, anchorOffset)
+      range = document.createRange()
+      range.collapse(true)
     }
-  })
+    tempRange = document.createRange()
+    tempRange.selectNodeContents(elem)
+    tempRange.setEnd(range.startContainer, range.startOffset)
+    prefix = tempRange.toString()
+
+    tempRange.selectNodeContents(elem)
+    tempRange.setStart(range.endContainer, range.endOffset)
+    suffix = tempRange.toString()
+
+    tempRange.detach()
+  } else if ( (sel = document.selection) && sel.type != 'Control') {
+    range = sel.createRange()
+    tempRange = document.body.createTextRange()
+    selected = tempRange.text
+
+    tempRange.moveToElementText(elem)
+    tempRange.setEndPoint('EndToStart', range)
+    prefix = tempRange.text
+
+    tempRange.moveToElementText(elem)
+    tempRange.setEndPoint('StartToEnd', range)
+    suffix = tempRange.text
+  }
+
+  console.info('extracted', [prefix, selected, suffix])
+
+  if (callback)
+    callback({ prefix, selected, suffix }, sel)
+  else
+    return { prefix, selected, suffix }
 }
 
-export let setSelection = (elem, prefixLen, rngLen) => {
-  let node
-  if (elem.childNodes[0]) {
-    node = elem.childNodes[0]
+const getTextNodesIn = (node) => {
+  var textNodes = []
+  if (node.nodeType == 3) {
+    textNodes.push(node)
   } else {
-    node = elem
-    prefixLen, rngLen = 0
+    var children = node.childNodes;
+    for (var i = 0, len = children.length; i < len; ++i) {
+      textNodes.push.apply(textNodes, getTextNodesIn(children[i]))
+    }
   }
-  let selection = document.getSelection()
-  let range = document.createRange()
-  range.setStart(node, prefixLen)
-  range.setEnd(node, prefixLen + rngLen)
-  selection.removeAllRanges()
-  selection.addRange(range)
+  return textNodes
+}
+
+export const setSelection = (elem, start, end) => {
+  if (document.createRange && window.getSelection) {
+    var range = document.createRange()
+    range.selectNodeContents(elem)
+    var textNodes = getTextNodesIn(elem)
+    var foundStart = false
+    var charCount = 0, endCharCount
+
+    for (var i = 0, textNode; textNode = textNodes[i++]; ) {
+      endCharCount = charCount + textNode.length
+      if (!foundStart && start >= charCount && (start < endCharCount || (start == endCharCount && i <= textNodes.length))) {
+        range.setStart(textNode, start - charCount)
+        foundStart = true
+      }
+      if (foundStart && end <= endCharCount) {
+        range.setEnd(textNode, end - charCount)
+        break
+      }
+      charCount = endCharCount
+    }
+
+    var sel = window.getSelection();
+    sel.removeAllRanges();
+    sel.addRange(range);
+  } else if (document.selection && document.body.createTextRange) {
+    var textRange = document.body.createTextRange()
+    textRange.moveToElementText(elem)
+    textRange.collapse(true)
+    textRange.moveEnd('character', end)
+    textRange.moveStart('character', start)
+    textRange.select()
+  }
 }
